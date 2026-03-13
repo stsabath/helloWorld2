800cded574a398ad44459b1c059f91ed4945aef0
stsabath <stsabath@terpmail.umd.edu>
2026-03-13 03:59:34 -0400

    Commiting base, contact, and favorite-course html's

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -18,6 +18,39 @@ def about():  # put application's code here
 @app.route('/about-css')
 def about_css():  # put application's code here
     return render_template('about-css.html')
+
+@app.route('/favorite-course')
+def favorite_course():
+
+    subject = request.args.get('subject')
+    course_number = request.args.get('course_number')
+
+    return render_template(
+        "favorite-course.html",
+        subject=subject,
+        course_number=course_number
+    )
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+
+    if request.method == 'POST':
+
+        first_name = request.form.get('first_name')
+        last_name = request.form.get('last_name')
+        email = request.form.get('email')
+        message = request.form.get('message')
+
+        return render_template(
+            "contact.html",
+            submitted=True,
+            first_name=first_name,
+            last_name=last_name,
+            email=email,
+            message=message
+        )
+
+    return render_template("contact.html")
 
 if __name__ == '__main__':
     app.run()
